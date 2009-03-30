d94b1923b001247f7ac8e532c7b3838e088af8f5
Thomas Kadauke <tkadauke@imedo.de>
2009-03-30 12:12:35 +0200

    separated version of demo application

--- shell/config/routes.rb
+++ shell/config/routes.rb
@@ -1,8 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
-  # These are commented out, because they dispatch requests to the other applications.
+  # These dispatch requests to the other applications.
   
-  # map.connect 'blog_entries/*params', :controller => 'apps', :action => 'dispatch', :app => 'blog', :app_controller => 'blog_entries'
-  # map.connect 'comments/*params', :controller => 'apps', :action => 'dispatch', :app => 'comments', :app_controller => 'comments'
+  map.connect 'blog_entries/*params', :controller => 'apps', :action => 'dispatch', :app => 'blog', :app_controller => 'blog_entries'
+  map.connect 'comments/*params', :controller => 'apps', :action => 'dispatch', :app => 'comments', :app_controller => 'comments'
 
   # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'
